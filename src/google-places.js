eeaca90a128adeafb93e87dbbe476385b4d600c1
Arun Israel <arun.israel@gmail.com>
2013-09-17 23:32:22 -0700

    clean up tests; prettify src code

--- src/google-places.js
+++ src/google-places.js
@@ -1,111 +1,103 @@
 'use strict';
 
-angular.module('gPlaces',[]);
+angular.module('gPlaces', []);
 
 angular.module('gPlaces').
-  provider('gPlacesAPI', function() {
+provider('gPlacesAPI', function () {
 
     this.options = {
-      nearbySearchURL:'https://maps.googleapis.com/maps/api/place/nearbysearch',
-      placeDetailsURL:'https://maps.googleapis.com/maps/api/place/details',
-      key:null,
-      radius:1000,
-      sensor:false,
-      location:{
-        latitude:null,
-        longitude:null
-      },
-      format:'json',
-      types:'food'
+        nearbySearchURL: 'https://maps.googleapis.com/maps/api/place/nearbysearch',
+        placeDetailsURL: 'https://maps.googleapis.com/maps/api/place/details',
+        key: null,
+        radius: 1000,
+        sensor: false,
+        location: {
+            latitude: null,
+            longitude: null
+        },
+        format: 'json',
+        types: 'food'
     }
 
-    this.buildParams = function(keys,options) {
-      var obj = {};
-      angular.forEach(keys,function(opt){
-        obj[opt] = options[opt];
-      });
-      return obj;
-    }
-
-    this.parseNSJSON = function(response) {
-      var pResp = [];
-      var keys = [
-                  'name',
-                  'reference',
-                  'vicinity'
-                ];
-      response['results'].map(function(result){
-        var obj = {}
-        angular.forEach(keys,function(k){
-          obj[k] = result[k];
+    this.buildParams = function (keys, options) {
+        var obj = {};
+        angular.forEach(keys, function (opt) {
+            obj[opt] = options[opt];
         });
-        pResp.push(obj);
-      })
-      return pResp;
+        return obj;
     }
 
-    this.parsePDJSON = function(response) {
-      var pResp = {};
-      var keys = [
-                  'formatted_address',
-                  'formatted_phone_number',
-                  'reference',
-                  'website'
-                  ];
-      angular.forEach(keys,function(k){
-        pResp[k] = response['result'][k];
-      });
-      return pResp;
+    this.parseNSJSON = function (response) {
+        var pResp = [];
+        var keys = ['name', 'reference', 'vicinity'];
+        response['results'].map(function (result) {
+            var obj = {}
+            angular.forEach(keys, function (k) {
+                obj[k] = result[k];
+            });
+            pResp.push(obj);
+        })
+        return pResp;
     }
 
-    this.$get = function($http,$q) {
+    this.parsePDJSON = function (response) {
+        var pResp = {};
+        var keys = ['formatted_address', 'formatted_phone_number',
+            'reference', 'website'
+        ];
+        angular.forEach(keys, function (k) {
+            pResp[k] = response['result'][k];
+        });
+        return pResp;
+    }
+
+    this.$get = function ($http, $q) {
         var opts = this.options;
         var buildParams = this.buildParams;
         var parseNSJSON = this.parseNSJSON;
         var parsePDJSON = this.parsePDJSON;
         return {
-            getOptions: function() {
-              return opts;
+            getOptions: function () {
+                return opts;
             },
-            placeSearch: function(cparams) {
-              var popts = angular.extend(opts,cparams);
-              var url = popts['nearbySearchURL'] + '/' + popts['format'];
-              var location = {location: popts['latitude'] + ',' + popts['longitude']};
-              var deferred = $q.defer();
-              $http.get(url, {params: angular.extend(buildParams(['radius',
-                'types',
-                'names',
-                'sensor',
-                'key'],popts)
-              ,location)})
-              .success(function(data){
-                return deferred.resolve(parseNSJSON(data));
-              })
-              .error(function(){
-                deferred.reject('error finding nearby places');
-              });
-              return deferred.promise;
+            placeSearch: function (cparams) {
+                var popts = angular.extend(opts, cparams);
+                var url = popts['nearbySearchURL'] + '/' + popts['format'];
+                var location = {
+                    location: popts['latitude'] + ',' + popts['longitude']
+                };
+                var deferred = $q.defer();
+                $http.get(url, {
+                    params: angular.extend(buildParams(['radius',
+                        'types', 'names', 'sensor', 'key'
+                    ], popts), location)
+                }).success(function (data) {
+                    return deferred.resolve(parseNSJSON(data));
+                }).error(function () {
+                    deferred.reject('error finding nearby places');
+                });
+                return deferred.promise;
             },
-            placeDetails:function(cparams){
-              var popts = angular.extend(opts,cparams);
-              var url = popts['placeDetailsURL'] + '/' + popts['format'];
-              var deferred = $q.defer();
-              $http.get(url, {params: buildParams(['reference',
-                'sensor',
-                'key'],popts)})
-              .success(function(data){
-                deferred.resolve(parsePDJSON(data));
-              })
-              .error(function(){
-                deferred.reject('error grabbing place details');
-              });
-              return deferred.promise;
+
+            placeDetails: function (cparams) {
+                var popts = angular.extend(opts, cparams);
+                var url = popts['placeDetailsURL'] + '/' + popts['format'];
+                var deferred = $q.defer();
+                $http.get(url, {
+                    params: buildParams(['reference', 'sensor', 'key'],
+                        popts)
+                }).success(function (data) {
+                    deferred.resolve(parsePDJSON(data));
+                }).error(function () {
+                    deferred.reject('error grabbing place details');
+                });
+                return deferred.promise;
             }
         }
     };
 
-    this.setOptions = function(opts) {
-        angular.extend(this.options,opts);
+    this.setOptions = function (opts) {
+        angular.extend(this.options, opts);
     };
 
 });
--- test/google-places.js
+++ test/google-places.js
@@ -1,63 +1,109 @@
 'use strict';
 
 describe('Service: gPlacesAPI', function () {
-  var gPlacesAPI, opts, $httpBackend;
-  var nearbySearchURL = 'https://maps.googleapis.com/maps/api/place'
-  + '/nearbysearch/json?key=test&location=10,-20&radius=1000&'
-  + 'sensor=false&types=food';
+    var gPlacesAPI;
 
-  var placeDetailsURL = 'https://maps.googleapis.com/maps/api/place'
-  + '/details/json?key=test&reference=reference_id&sensor=false';
+    beforeEach(module('gPlaces', function (gPlacesAPIProvider) {
+        gPlacesAPIProvider.setOptions({
+            key: 'test'
+        });
+    }));
 
-  beforeEach(module('gPlaces','mockedNearbySearch','mockedPlaceDetails',
-    function(gPlacesAPIProvider){
-      gPlacesAPIProvider.setOptions({key:'test'});
-    }
-  ));
+    describe('Option Parsing', function () {
+        var opts;
 
-  beforeEach(inject(function (_gPlacesAPI_,_$httpBackend_,defaultNSJSON,defaultPDJSON) {
-    gPlacesAPI = _gPlacesAPI_;
-    opts = gPlacesAPI.getOptions();
-    $httpBackend = _$httpBackend_;
-    $httpBackend.whenGET(nearbySearchURL).respond(defaultNSJSON);
-    $httpBackend.whenGET(placeDetailsURL).respond(defaultPDJSON);
-  }));
+        beforeEach(inject(function (_gPlacesAPI_) {
+            gPlacesAPI = _gPlacesAPI_;
+            opts = gPlacesAPI.getOptions();
+        }));
 
-  afterEach(function() {
-    $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequest();
-  });
+        it('should return default option for radius', function () {
+            expect(opts.radius).toEqual(1000);
+        });
 
-  it('should return default option for radius', function () {
-    expect(opts.radius).toEqual(1000);
-  });
-
-  it('should return custom option for key', function () {
-    expect(opts.key).toEqual('test');
-  });
-
-  it('should return nearby places for a location', function () {
-    var results;
-    gPlacesAPI.placeSearch({latitude:10,longitude:-20}).then(function(data){
-      results = data;
+        it('should return custom option for key', function () {
+            expect(opts.key).toEqual('test');
+        });
     });
-    $httpBackend.flush();
-    expect(results.length).toEqual(3);
-    expect(results[0].name).not.toBeUndefined();
-    expect(results[1].vicinity).not.toBeUndefined();
-    expect(results[2].reference).not.toBeUndefined();
-  });
 
-  it('should return place details', function () {
-    var results;
-    gPlacesAPI.placeDetails({reference:'reference_id'}).then(function(data){
-      results = data;
+    describe('Nearby Search', function () {
+        var $httpBackend;
+
+        var nearbySearchURL =
+            'https://maps.googleapis.com/maps/api/place' +
+            '/nearbysearch/json?key=test&location=10,-20&radius=1000&' +
+            'sensor=false&types=food';
+
+        beforeEach(module('mockedNearbySearch'));
+
+        beforeEach(inject(function (_gPlacesAPI_, _$httpBackend_,
+            defaultNSJSON) {
+            gPlacesAPI = _gPlacesAPI_;
+            $httpBackend = _$httpBackend_;
+            $httpBackend.whenGET(nearbySearchURL).respond(
+                defaultNSJSON);
+        }));
+
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should return nearby places for a location', function () {
+            var results;
+            gPlacesAPI.placeSearch({
+                latitude: 10,
+                longitude: -20
+            }).then(function (data) {
+                results = data;
+            });
+            $httpBackend.flush();
+            expect(results.length).toEqual(3);
+            expect(results[0].name).not.toBeUndefined();
+            expect(results[1].vicinity).not.toBeUndefined();
+            expect(results[2].reference).not.toBeUndefined();
+        });
     });
-    $httpBackend.flush();
-    expect(results.formatted_address).toEqual('37 Rio Robles East, San Jose, CA, United States');
-    expect(results.formatted_phone_number).toEqual('(408) 577-0300');
-    expect(results.website).toEqual('http://www.quiznos.com/restaurants/CA/San%20Jose');
-    expect(results.reference).not.toBeUndefined();
-  });
+
+
+    describe('Place Details Search', function () {
+        var $httpBackend;
+
+        var placeDetailsURL =
+            'https://maps.googleapis.com/maps/api/place' +
+            '/details/json?key=test&reference=reference_id&sensor=false';
+
+        beforeEach(module('mockedPlaceDetails'));
+
+        beforeEach(inject(function (_gPlacesAPI_, _$httpBackend_,
+            defaultPDJSON) {
+            gPlacesAPI = _gPlacesAPI_;
+            $httpBackend = _$httpBackend_;
+            $httpBackend.whenGET(placeDetailsURL).respond(
+                defaultPDJSON);
+        }));
+
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should return place details', function () {
+            var result;
+            gPlacesAPI.placeDetails({
+                reference: 'reference_id'
+            }).then(function (data) {
+                result = data;
+            });
+            $httpBackend.flush();
+            expect(result.formatted_address).toEqual(
+                '37 Rio Robles East, San Jose, CA, United States');
+            expect(result.formatted_phone_number).toEqual(
+                '(408) 577-0300');
+            expect(result.website).toEqual(
+                'http://www.quiznos.com/restaurants/CA/San%20Jose');
+            expect(result.reference).not.toBeUndefined();
+        });
+    });
 
 });
