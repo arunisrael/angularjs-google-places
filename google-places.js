81baec1c062a97f4f33324cad6d0fcd0a60ad9bf
Justin <justinbchristensen@gmail.com>
2014-10-02 12:23:26 -0400

    modified and added custom places file to to a text search

---
+++ google-places.js
@@ -0,0 +1,133 @@
+'use strict';
+
+angular.module('ngGPlaces', []);
+angular.module('ngGPlaces').value('gPlaces', google.maps.places);
+angular.module('ngGPlaces').value('gMaps', google.maps);
+
+angular.module('ngGPlaces').
+provider('ngGPlacesAPI', function () {
+
+    var defaults = {
+        radius: 1000,
+        sensor: false,
+        latitude: null,
+        longitude: null,
+        // types: ['lodging'],
+        map: null,
+        elem: null,
+        textSearchKeys: ['formatted_address','geometry','id','name','place_id','price_level','rating','reference','types'],
+        nearbySearchKeys: ['geometry','id','name','place_id','rating','reference','types'],
+        placeDetailsKeys: ['formatted_address','formatted_phone_number','geometry','html_attributions','icon','id','international_phone_number','name','opening_hours','photos','place_id','price_level','rating','reference','reviews','types', 'vicinity','website'],
+        textSearchErr: 'Unable to find by text',
+        nearbySearchErr: 'Unable to find nearby places',
+        placeDetailsErr: 'Unable to find place details',
+        _textSearchApiFnCall: 'textSearch',
+        _nearbySearchApiFnCall: 'nearbySearch',
+        _placeDetailsApiFnCall: 'getDetails'
+    };
+
+    var parseNSJSON = function (response) {
+        var pResp = [];
+        var keys = defaults.nearbySearchKeys;
+        response.map(function (result) {
+            var obj = {};
+            angular.forEach(keys, function (k) {
+                obj[k] = result[k];
+            });
+            pResp.push(obj);
+        });
+        return pResp;
+    };
+
+  var parseTSJSON = function (response) {
+        var pResp = [];
+        var keys = defaults.textSearchKeys;
+        response.map(function (result) {
+            var obj = {};
+            angular.forEach(keys, function (k) {
+                obj[k] = result[k];
+            });
+            pResp.push(obj);
+        });
+        return pResp;
+    };
+
+    var parsePDJSON = function (response) {
+        var pResp = {};
+        var keys = defaults.placeDetailsKeys;
+        angular.forEach(keys, function (k) {
+            pResp[k] = response[k];
+        });
+        return pResp;
+    };
+
+    this.$get = function ($rootScope, $q, gMaps, gPlaces, $window) {
+        function commonAPI(args) {
+            var req = angular.copy(defaults, {});
+            angular.extend(req, args);
+            var deferred = $q.defer();
+            var elem, service;
+
+            function callback(results, status) {
+                if (status == gPlaces.PlacesServiceStatus.OK) {
+                    $rootScope.$apply(function () {
+                        return deferred.resolve(req._parser(results));
+                    });
+                } else {
+                    $rootScope.$apply(function () {
+                        deferred.reject(req._errorMsg);
+                    });
+                }
+            }
+            if (req._genLocation) {
+                req.location = new gMaps.LatLng(req.latitude, req.longitude);
+            }
+            if (req.map) {
+                elem = req.map;
+            } else if (req.elem) {
+                elem = req.elem;
+            } else {
+                elem = $window.document.createElement('div');
+            }
+            service = new gPlaces.PlacesService(elem);
+            service[req._apiFnCall](req, callback);
+            return deferred.promise;
+        }
+
+        return {
+            getDefaults: function () {
+                return defaults;
+            },
+            nearbySearch: function (args) {
+                args._genLocation = true;
+                args._errorMsg = defaults.nearbySearchErr;
+                args._parser = parseNSJSON;
+                args._apiFnCall = defaults._nearbySearchApiFnCall;
+                return commonAPI(args);
+            },
+            textSearch: function(args) {
+                args._genLocation = true;
+                args._errorMsg = defaults.textSearchErr;
+                args._parser = parseTSJSON;
+                args._apiFnCall = defaults._textSearchApiFnCall;
+                return commonAPI(args);  
+            },
+            placeDetails: function (args) {
+                args._errorMsg = defaults.placeDetailsErr;
+                args._parser = parsePDJSON;
+                args._apiFnCall = defaults._placeDetailsApiFnCall;
+                return commonAPI(args);
+            },
+            setDefaults: function(args) {
+                angular.extend(defaults, args);
+            }
+        };
+    };
+
+    this.$get.$inject = ['$rootScope', '$q', 'gMaps', 'gPlaces', '$window'];
+
+    this.setDefaults = function (args) {
+        angular.extend(defaults, args);
+    };
+
+});
