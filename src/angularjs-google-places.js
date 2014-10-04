9949ba1714ac9d38bc66c8aec5aeda7ffb5d4765
Justin <justinbchristensen@gmail.com>
2014-10-04 17:04:46 -0400

    fixed radar search

--- src/angularjs-google-places.js
+++ src/angularjs-google-places.js
@@ -18,7 +18,7 @@ provider('ngGPlacesAPI', function () {
         textSearchKeys: ['formatted_address','geometry','id','name','place_id','price_level','rating','reference','types'],
         nearbySearchKeys: ['geometry','id','name','place_id','rating','reference','types'],
         placeDetailsKeys: ['formatted_address','formatted_phone_number','geometry','html_attributions','icon','id','international_phone_number','name','opening_hours','photos','place_id','price_level','rating','reference','reviews','types', 'vicinity','website'],
-        radarSearchKeys: [],
+        radarSearchKeys: ['geometry', 'place_id'],
         nearbySearchErr: 'Unable to find nearby places',
         placeDetailsErr: 'Unable to find place details',
         radarSearchErr: 'Unable to find search details',
